8091ad323a056a4cec42fddad33739ffd4e9f6fa
Elchin <Elchin@DESKTOP-HMRVE90>
2024-10-19 17:23:58 +0400

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ip = IPAddress.Loopback;
+            var port = 27001;
+            var ep = new IPEndPoint(ip, port);
+            Console.WriteLine("Client");
+            var client = new TcpClient();
+
+            try
+            {
+                client.Connect(ep);
+                if (client.Connected)
+                {
+                    Console.Write("Enter file path: ");
+                    var filePath = Console.ReadLine()!;
+                    using (var fs = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Read))
+                    {
+                        var fileName = Path.GetFileName(filePath);
+                        var networkStream = client.GetStream();
+                        var fileNameBytes = Encoding.UTF8.GetBytes(fileName + "\n");
+                        networkStream.Write(fileNameBytes, 0, fileNameBytes.Length);
+                        var bytes=new byte[1024];
+                        var len = 0;
+
+
+                        while ((len = fs.Read(bytes, 0, bytes.Length)) > 0)
+                        {
+                            networkStream.Write(bytes, 0, len);
+                        }
+
+                        networkStream.Close();
+                        client.Close();
+                    }
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Server Client File/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server_Client_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var ip = IPAddress.Loopback;
+            var port = 27001;
+            var ep = new IPEndPoint(ip, port);
+            var listener = new TcpListener(ep);
+            Console.WriteLine("Server");
+            
+            try
+            {
+                listener.Start();
+                while (true)
+                {
+                    var client= listener.AcceptTcpClient();
+                    _ = Task.Run(() =>
+                    {
+                        Console.WriteLine($"{client.Client.RemoteEndPoint} is connected");
+                        var networkStream = client.GetStream();
+                        var remoteEp = client.Client.RemoteEndPoint as IPEndPoint;
+                        var directoryPath = Path.Combine(Environment.CurrentDirectory, remoteEp!.Address.ToString());
+
+                        if (!Directory.Exists(directoryPath))
+                            Directory.CreateDirectory(directoryPath);
+
+                        var buffer = new byte[1024];
+                        var fileNameBuilder = new StringBuilder();
+                        int bytesRead;
+                        while ((bytesRead = networkStream.Read(buffer, 0, 1)) > 0)
+                        {
+                            var character = Encoding.UTF8.GetString(buffer, 0, 1);
+                            if (character == "\n")
+                                break;
+                            fileNameBuilder.Append(character);
+                        }
+
+                        var fileName = Path.Combine(directoryPath, fileNameBuilder.ToString().Trim());
+
+                        using (var fs = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write))
+                        {
+                            int len = 0;
+                            var bytes = new byte[1024];
+
+                            while ((len = networkStream.Read(bytes, 0, bytes.Length)) > 0)
+                            {
+                                fs.Write(bytes, 0, len);
+                            }
+                        };
+
+                        Console.WriteLine("File recieved");
+                        client.Close();
+
+                    });
+
+
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
